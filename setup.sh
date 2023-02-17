aa12d5319c7053fabccfdf903f20494d78f43540
ykyk3 <sakura198606@gmail.com>
2023-02-17 22:56:03 +0900

    セットアップの変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-DOT_FILES=(.bashrc .bash_profile .git .vim .pyrc .env-exporter .dein .gitconfig .config/nvim/init.vim .tmux.conf .config/fish)
-
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
+# chezmoiを先にinstallする必要がある
+chezmoi init --apply ykyk3
